c7ddfe1ff663f6e7a3bf238668ef12c2500f4de1
Searchstars <searchstars@163.com>
2026-05-03 00:46:07 +0800

    Revert "更新依赖"
    This reverts commit d957935ec4e38fc4362653d4fb2a74d6ae2d3c78.

--- src/spp/xiaomi.rs
+++ src/spp/xiaomi.rs
@@ -191,18 +191,14 @@ impl XiaomiSpp {
             sar_version,
             connect_type,
             Some(tx_win_overrun_allowance),
-            None,
-            None,
             false,
             {
                 let tx = tx.clone();
-                move |data: Vec<Vec<u8>>| {
+                move |data: Vec<u8>| {
                     let tx = tx.clone();
                     async move {
                         //log::info!("[wasm] Send: {}", corelib::tools::to_hex_string(&data));
-                        for chunk in data {
-                            let _ = tx.send(chunk).await;
-                        }
+                        let _ = tx.send(data).await;
                         Ok(())
                     }
                 }
